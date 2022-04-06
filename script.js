08bb5ea35f4743ff3ef58945b160d612c316f4d4
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-06 19:13:15 -0300

    greatest of two numbers program

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function module (elementA, elementB) {
-    let results = elementA % elementB;
-    return results
+function greatestOfTwo( elementA,elementB) {
+   let result = elementA > elementB ? elementA : elementB;
+        return result
+    
 }
 
-console.log(module(5,10))
+console.log(greatestOfTwo(25,48))
