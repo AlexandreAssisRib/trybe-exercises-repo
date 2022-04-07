97758b35c1c36900e653a14263b3798ac04bf80d
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-07 19:19:18 -0300

    exercise of day 4.2 -number 5.

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 let numbers = [5, 9, 3, 19, 70, 8, 2, 35, 27];
 
-let result = AverageArray(numbers)
-console.log(result)
-function AverageArray (arrayOfnums) {
-    sum = 0;
-    for (let i =0; i < numbers.length; i++) {
-       sum = sum + (arrayOfnums[i]);
-    
-    }
-    let average = sum/numbers.length;
-    let resul = average>20 ? 'valor maior que 20': 'valor menor que 20';
-    return resul + ' :' + average
-    
-}
+let maxInArray = numbers.reduce((el,total) => {return Math.max(el,total) })
+console.log(maxInArray)
