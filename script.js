77b74799e074240c13cc9670ef12f74c994a95a4
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-07 09:15:36 -0300

    the nums is even ? true or false program

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
+let result = theNumIsEven(12,15,11)
+console.log(result)
 
-let input = 20;
-let result = parseInt(input)
-console.log(noteConverter(result))
-function noteConverter(element) {
-    if ( element >= 90) {
-        return("A")
-    }
-    else if ( element>=80 && element<90) {
-        return ("B")
-    }
-    else if( element>=70 && element<80) {
-        return ("C")
-    }
-    else if ( element>=60 && element<70) {
-        return ("D")
-    }
-    else if ( element >= 50 && element<60) {
-        return("E")
-    }
-    else if( element < 50 && element>0) {
-        return("F")
-    } else {
-        return("error!")
-    }
+function theNumIsEven(paramsA, paramsB,paramsC) {
+if ( paramsA%2 == 0 || paramsB%2 == 0 || paramsC%2 == 0) {
+    return true;
+} else {
+    return false;
+}
+    
 }
