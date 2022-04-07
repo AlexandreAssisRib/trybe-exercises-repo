44007117c01eba1e12a4c269d5db8764017a1063
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-07 15:24:26 -0300

    exercise 4 - day 2 bloc 4

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push("Contato")
-console.log(menu);
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let i = 0; i < groceryList.length; i++) {
+    console.log(groceryList[i])
+}
