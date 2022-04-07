c9537127fe42de4f7d028022639306f2e7a7b2f2
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-07 18:34:14 -0300

    exercise of day 4.2 - number 2

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for ( let i =0; i < numbers.length; i++) {
-    console.log(numbers[i])
+sumArray(numbers)
+
+function sumArray(arrayOfnums) {
+    sum = 0;
+    for (let i =0; i < numbers.length; i++) {
+       sum = sum + (arrayOfnums[i])
+    }
+console.log(sum)
 }
