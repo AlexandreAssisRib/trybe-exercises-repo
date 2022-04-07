debcd98cd183a08cc57bd7f08fe140e6bf87a14a
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-07 09:21:40 -0300

    the nums is odd? true or false program

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let result = theNumIsEven(12,15,11)
+let result = theNumIsOdd(12,13,18)
 console.log(result)
 
-function theNumIsEven(paramsA, paramsB,paramsC) {
-if ( paramsA%2 == 0 || paramsB%2 == 0 || paramsC%2 == 0) {
+function theNumIsOdd(paramsA, paramsB,paramsC) {
+if ( paramsA%2 !== 0 || paramsB%2 !== 0 || paramsC%2 !== 0) {
     return true;
 } else {
     return false;
