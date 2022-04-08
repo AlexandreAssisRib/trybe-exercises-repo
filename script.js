ff1d6c2ced014caf1b436565cb803cf879b892af
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-08 12:49:20 -0300

    exercises of day 4.2 - exercise 8

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let smaller = numbers[0];
-for (let i=1;i<numbers.length;i++) {
-    if (numbers[i] < smaller) {
-        smaller = numbers[i]
-    }
+let array = [];
+for(let i =0; i<26;i++) {
+    array.push(i)
 }
-console.log(smaller)
+console.log(array)
