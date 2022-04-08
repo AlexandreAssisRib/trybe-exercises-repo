64360c58fdf849ff09c72de057dead50bee21e72
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-08 19:50:44 -0300

    4.3 - exercise 2.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-factorial(10)
-function factorial (entrada) {
-    let mult = 1;
-    for(let i = entrada;i>0;i--) {
-        mult = mult * i ;
-        
+let word = 'tryber'
+
+
+let result = wordInverter(word)
+function wordInverter(word) {
+    let wordinverted = " "
+    for( let i = word.length-1; i>=0;i--) {
+        wordinverted += word[i];
+       
     }
-   console.log(mult)
+    console.log(wordinverted)
 }
