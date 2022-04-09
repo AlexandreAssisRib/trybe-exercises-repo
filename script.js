94a0cf713a8420ec9d69e83b475da29282ffb683
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-09 09:53:08 -0300

    return the greatest word in the array

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-let word = 'tryber'
-
-
-let result = wordInverter(word)
-function wordInverter(word) {
-    let wordinverted = " "
-    for( let i = word.length-1; i>=0;i--) {
-        wordinverted += word[i];
-       
+let array = ['java', 'javascript', 'python', 'html', 'css', 'ajsiajeswuwhdwuqduw'];
+let result = theGreatestWord(array)
+console.log(result)
+function theGreatestWord(array) {
+    let word = []
+    for (let i = 0; i<=array.length-1;i++) {
+        word.push(array[i].length)
+        // 
+   }
+let num = word[0];
+for ( let x =1; x < word.length; x++) {
+    if(word[x]>num) {
+        num = word[x]
     }
-    console.log(wordinverted)
+
 }
+    let results = word.indexOf(num)
+    return array[results]
+}
+
+    
+    
+
