8eb60bec7d5bcccb13941deb920fa7121e0f8b69
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-12 11:35:48 -0300

    exercises part 1 - number 1

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-theGreatestNumPrime(50)
-
-function theGreatestNumPrime(entrada) {
-    let numsInArray = [];
-    let greatest;
-    for( let num = 2; num <=entrada;num++) {
-        let ehprimo=true;
-        for(let div = 2; div < num; div++) {
-            if( num%div === 0) {
-                ehprimo=false;
-                break;
-            }
-        }
-    if(ehprimo) {
-        numsInArray.push(num);
-    } 
-    greatest = numsInArray[0] ;
-    for(let i = 0; i<numsInArray.length; i++) {
-        if( numsInArray[i]>greatest) {
-            greatest = numsInArray[i];
-        }
-        
-    }
-    
-    }
-    console.log(greatest);
-}
-
-
-    
-    
-
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+console.log("bem vinda " + info.personagem)
