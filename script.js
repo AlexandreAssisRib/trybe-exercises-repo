a0ca8621030b8aeee902e809c3baa8d9c9d41c40
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-18 20:17:07 -0300

    exercise 1 - Ok!

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+DaysOfMonth(dezDaysList)
+
+function DaysOfMonth(array) {
+    let days = document.getElementById('days');
+    
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+
+      
+      
+      let listOfDays = document.createElement('li');
+
+     
+
+      listOfDays.innerText = element;
+      
+      if(array[index] === 24 || array[index] === 25 || array[index === 31]) {
+        listOfDays.className = 'days holiday'
+      }
+      if ( array[index] === 4 || array[index] === 11 || array[index] === 18 || array[index] === 25) { 
+        listOfDays.className = 'days friday'
+
+      } 
+      else {
+        listOfDays.className = 'days'
+        
+      }
+      days.appendChild(listOfDays)
+
+      console.log(listOfDays)
+      
+    }
+    
+    
+}
+
+
+
+  
+
+
