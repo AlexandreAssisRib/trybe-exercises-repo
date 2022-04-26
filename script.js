35f1cb021995ce2a8f0ba09150bea1087487bc47
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-26 19:49:50 -0300

    formulario html finalizado

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    let butao = document.getElementById('enviar')
+    console.log(butao)
+    butao.addEventListener('click', function(event){
+    event.preventDefault()
+    })
+
+}
