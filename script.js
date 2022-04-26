ff309ce2f6c39157c3b225c678c029807ef91c6e
AlexandreAssisRib <alexandre_vga@hotmail.com>
2022-04-26 20:02:09 -0300

    formulario - introduce del buttun

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ window.onload = function () {
     event.preventDefault()
     })
 
+    let apagar = document.getElementById('apagar');
+    apagar.addEventListener('click',function(event){
+    event.preventDefault();
+    let inputs = document.getElementsByTagName('input')
+    for (let index = 0; index < inputs.length; index++) {
+       inputs[index].value = ''
+        
+    }
+
+    })
 }
